ad5d8aaa66712fc9cb9d4e25c417bf54e7ec78d5
luciano <nicolini.luciano@hotmail.com>
2022-04-11 19:16:46 -0300

    CardViewdDemo1
    //Card view personalizado para reutilizar en diferentes proyectos.

---
+++ CardViewDemo/Shared/CardViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewDemoApp.swift
+//  Shared
+//
+//  Created by Luciano Nicolini on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CardViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardViewDemo/Shared/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Luciano Nicolini on 11/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Spacer()
+            Image("turtlerock_feature")
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode:.fill)
+                .frame(width:300,height:250)
+                .cornerRadius(20.0)
+                
+            Text("Hotel Metropol ")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            //.primary,.primary.opacity(0.5)
+                .foregroundStyle(.linearGradient(colors: [.red,.orange], startPoint: .topLeading, endPoint: .bottomTrailing))
+          
+            Text("Mar del plata".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(.secondary)
+                
+        }
+        .padding(.all,20.0)
+        .padding(.vertical,20)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        //.cornerRadius(30.0)
+        //.mask(RoundedRectangle(cornerRadius: 30,style: .continuous))
+        //borde lineas blancas
+        .overlay(
+            RoundedRectangle(cornerRadius: 30,style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.3),.black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                .blendMode(.overlay)
+        )
+        
+        .shadow(color: .primary.opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal,20)
+        //.offset(x: 250, y: -100)
+        .background(Image("Blob 1"))
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
